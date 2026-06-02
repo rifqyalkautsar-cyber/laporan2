9e1b8e59b9a74817c159a82914612ad4cb16dae8
Rifqy135 <rifqyalkautsar06@gmail.com>
2026-06-02 22:40:20 +0800

    Selesaikan fitur autentikasi dan otorisasi Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,23 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ItemController;
 
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('items', ItemController::class);
-// Endpoint API selesai dibuat
+// Rute Publik (Bebas diakses tanpa login)
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Rute Terlindungi (Wajib punya Token / Wajib Login)
+Route::middleware('auth:sanctum')->group(function () {
+    
+    // Rute yang bisa diakses user biasa
+    Route::apiResource('categories', CategoryController::class)->except(['destroy']);
+    Route::apiResource('items', ItemController::class)->except(['destroy']);
+
+    // Rute super ketat (Hanya ADMIN yang boleh DELETE)
+    Route::delete('categories/{category}', [CategoryController::class, 'destroy'])->middleware('role:admin');
+    Route::delete('items/{item}', [ItemController::class, 'destroy'])->middleware('role:admin');
+});
