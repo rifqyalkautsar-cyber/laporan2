7e0a0740f98be5612b0c44780bfda1bc0e481763
Rifqy135 <rifqyalkautsar06@gmail.com>
2026-06-08 21:24:35 +0800

    Add API versioning, BaseController, input sanitization & documentation

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ItemController;
 
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('items', ItemController::class);
-// Endpoint API selesai dibuat
+Route::prefix('v1')->group(function() {
+    
+    Route::post('register', 'App\Http\Controllers\AuthController@register');
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+
+    Route::middleware('auth:sanctum')->group(function() {
+        
+        // Categories
+        Route::apiResource('categories', 'App\Http\Controllers\CategoryController')->except(['destroy']);
+        Route::delete('categories/{category}', 'App\Http\Controllers\CategoryController@destroy')->middleware('role:admin');
+
+        // Items
+        Route::apiResource('items', 'App\Http\Controllers\ItemController')->except(['destroy']);
+        Route::delete('items/{item}', 'App\Http\Controllers\ItemController@destroy')->middleware('role:admin');
+        
+    });
+    
+});
